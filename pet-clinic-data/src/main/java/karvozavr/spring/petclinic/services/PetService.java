1e6f10db7fbc51d4589fd99591ca529f315dd97a
karvozavr <karvozavr@gmail.com>
2019-08-04 12:56:44 +0300

    Add base CrudService

--- pet-clinic-data/src/main/java/karvozavr/spring/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/karvozavr/spring/petclinic/services/PetService.java
@@ -2,12 +2,5 @@ package karvozavr.spring.petclinic.services;
 
 import karvozavr.spring.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
--- pet-clinic-data/src/main/java/karvozavr/spring/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/karvozavr/spring/petclinic/services/VetService.java
@@ -2,13 +2,5 @@ package karvozavr.spring.petclinic.services;
 
 import karvozavr.spring.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
